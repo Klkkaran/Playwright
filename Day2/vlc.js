e5d53d6fd8e35a6e83c26f0f64298b378ee8faee
Karan Kumar <89890147+Klkkaran@users.noreply.github.com>
2025-07-24 20:07:39 +0530

    Create vlc.js

---
+++ Day2/vlc.js
@@ -0,0 +1,10 @@
+const browserName = 'Chrome'
+function getBrowserName(){
+//   let browserName = 'Chrome'
+    if(browserName == 'Chrome'){
+        let browserName='firefox'
+        console.log("broswer",'' +browserName)
+    }
+    console.log("function",'' +browserName)
+}
+getBrowserName()
