ab43e9318eed6a3b41f94c26842135a680c839cc
Karan Kumar <89890147+Klkkaran@users.noreply.github.com>
2025-07-24 16:14:44 +0530

    Create Primitive-Datatypes.js

---
+++ Day1/Primitive-Datatypes.js
@@ -0,0 +1,16 @@
+let firstName = 'Karan'
+console.log(firstName)
+let companyName = "MarketSimplified";
+console.log(companyName);
+let mobileNumber = 9858749932
+console.log(mobileNumber)
+let isAutomation = true;
+console.log(isAutomation)
+let hasPlaywright;
+console.log(hasPlaywright)
+
+console.log(typeof firstName)
+console.log(typeof(companyName));
+console.log(typeof mobileNumber);
+console.log(typeof isAutomation);
+console.log(typeof(hasPlaywright))
