aad216b7259107f45fcfc3feb6d7f1eb178d8f7e
Karan Kumar <89890147+Klkkaran@users.noreply.github.com>
2025-07-24 16:28:19 +0530

    Update odd-even.js

--- Day2/odd-even.js
+++ Day2/odd-even.js
@@ -1,1 +1,10 @@
 
+function isOddOrEven(number){
+if(number%2 == 0){
+    console.log("given number is even",'' +number)
+}
+else{
+    console.log("given number is odd",'' +number)
+}
+}
+isOddOrEven(39)
