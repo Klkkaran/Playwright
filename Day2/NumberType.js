1da26ddba986a14146f2f8251dabaa54d4c293c2
Karan Kumar <89890147+Klkkaran@users.noreply.github.com>
2025-07-24 16:47:14 +0530

    Create NumberType.js

---
+++ Day2/NumberType.js
@@ -0,0 +1,18 @@
+
+function pnnumber(number){
+//let number = "karan"
+var number = -1
+if(number>0){
+    console.log("provided number was positive",''+number)
+}
+else if(number<0){
+    console.log("provided number was negative", ''+number)
+}
+else if(number == 0){
+    console.log("given number was Zero",''+number)
+}
+else{
+    console.log("invalid number")
+}
+}
+pnnumber()
