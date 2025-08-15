2258d9179cc0ea5969590e024bb35687695494db
Karan Kumar <89890147+Klkkaran@users.noreply.github.com>
2025-08-15 21:18:10 +0530

    Create fibonaic.ts

---
+++ Day7/fibonaic.ts
@@ -0,0 +1,25 @@
+function fibonacci(n:number):number{
+    if(n==0){
+        console.log("error")
+        return 0
+    }
+    if(n==1){
+        return 1
+    }
+    let a=0
+    let b=1
+    for(let i=0;i<=n;i++){
+        const c=a+b
+        console.log(c)
+        if(c<=2){
+            break
+        }
+        a=b
+        b=c
+ 
+    }
+    return b
+}
+console.log(fibonacci(2))
+console.log(fibonacci(0))
+console.log(fibonacci(1))
